5c2407ed7b942b22058eefff245766676348e23a
Irfan Maulana <maulana.irfan@tokopedia.com>
2021-05-24 11:06:22 +0700

    feat: slug

--- src/libs/supabase.js
+++ src/libs/supabase.js
@@ -1,4 +1,3 @@
-
 import { createClient } from '@supabase/supabase-js'
 
 export const supabase = createClient(
---
+++ src/pages/[slug].js
@@ -0,0 +1,57 @@
+import { Box, Stack, Button, Heading } from "@chakra-ui/react";
+
+import { Layout } from "../components/Layout";
+
+import { supabase } from "../libs/supabase";
+
+function SlugPage() {
+  return (
+    <Layout height="100vh">
+      <Box width="100%">
+        <Stack spacing={8} mx={"auto"} mt="20" py={12} px={6}>
+          <Stack align={"center"} spacing={2} direction={"column"}>
+            <Heading fontSize={"4xl"}>The link is not available</Heading>
+
+            <Button
+              rounded="full"
+              px={6}
+              color={"white"}
+              bgGradient="linear(to-l, #7928CA, #FF0080)"
+              _hover={{
+                bgGradient: "linear(to-r, #7928CA, #FF0080)",
+              }}
+              as={"a"}
+              href={"/"}
+            >
+              Go to homepage
+            </Button>
+          </Stack>
+        </Stack>
+      </Box>
+    </Layout>
+  );
+}
+
+export async function getServerSideProps({ params }) {
+  const slug = params.slug;
+  const { data } = await supabase
+    .from("urls")
+    .select("real_url,slug")
+    .eq("slug", slug)
+    .single();
+
+  if (data && data.real_url) {
+    return {
+      redirect: {
+        destination: data.real_url,
+        permanent: false,
+      },
+    };
+  }
+
+  return {
+    props: {},
+  };
+}
+
+export default SlugPage;
