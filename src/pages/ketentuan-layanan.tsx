75f1c3583192565dfe4d9e67d25722a5fbf603e4
Irfan Maulana <mazipanneh@gmail.com>
2021-06-06 06:05:35 +0700

    Update ketentuan-layanan.tsx

--- src/pages/ketentuan-layanan.tsx
+++ src/pages/ketentuan-layanan.tsx
@@ -20,7 +20,7 @@ import { Layout } from 'components/Layout/Layout'
 function Terms() {
   return (
     <Layout height="100vh">
-      <MetaHead title="Ketentuan Layanan di Ksana.in" />
+      <MetaHead title="Ketentuan Layanan | Ksana.in" />
       <VStack spacing={4} textAlign="center" as="section" mt="32">
         <VStack spacing={4} textAlign="center">
           <Heading
