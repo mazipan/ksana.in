4ea7b37170096471becee88b171fb2bc498031f6
Irfan Maulana <mazipanneh@gmail.com>
2021-06-06 06:07:25 +0700

    Update forget-password.tsx

--- src/pages/auth/forget-password.tsx
+++ src/pages/auth/forget-password.tsx
@@ -7,7 +7,7 @@ import { MetaHead } from 'components/MetaHead/MetaHead'
 function ForgetPasswordPage() {
   return (
     <LayoutAuth minH={'100vh'} bg={useColorModeValue('gray.50', 'gray.800')}>
-      <MetaHead title="Lupa password di Ksana.in" />
+      <MetaHead title="Lupa Password | Ksana.in" />
       <AuthForgetPassword />
     </LayoutAuth>
   )
