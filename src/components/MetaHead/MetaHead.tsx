17e8e40d399461b022d36f52968aab03e72efae9
Irfan Maulana <mazipanneh@gmail.com>
2021-07-01 21:35:19 +0700

    fix: remove no scalable

--- src/components/MetaHead/MetaHead.tsx
+++ src/components/MetaHead/MetaHead.tsx
@@ -20,7 +20,7 @@ export function MetaHead({
     <Head>
       <meta
         name="viewport"
-        content="minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, user-scalable=no, viewport-fit=cover"
+        content="minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, viewport-fit=cover"
       />
       <meta name="robots" content={robots} />
       <title>{title}</title>
