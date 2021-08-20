f928cb2450e66a268c2c5ecbdec7a0ccc49c73e1
Irfan Maulana (@mazipan) <maulana.irfan@tokopedia.com>
2021-08-20 09:41:49 +0700

    feat: add new search feature

--- src/components/Dashboard/UrlList/Items.tsx
+++ src/components/Dashboard/UrlList/Items.tsx
@@ -1,3 +1,4 @@
+import { ChangeEvent, useState , useEffect } from 'react'
 import { List } from '@chakra-ui/react'
 
 import useUrls from 'hooks/useUrls'
@@ -8,14 +9,36 @@ import { ErrorDataNotFound } from 'components/Error/ErrorDataNotFound'
 import { IUrl } from 'interfaces/IUrl'
 
 import { LoadingSkeleton } from './LoadingSkeleton'
+import { SearchInput } from './SearchInput'
 import { Item } from './Item'
 import { TotalStats } from './TotalStats'
 
 export function Items({ user, isFormVisible, onShowForm }: IUrlListProps) {
   const { data, isLoading } = useUrls(user?.id || '')
+  const [searchText, setSearchText] = useState<string>('')
+  const [filteredData, setFilteredData] = useState<IUrl[]>(data)
+
+  useEffect(() => {
+    if (!isLoading) {
+      if (searchText === '') {
+        setFilteredData(data)
+      } else if (searchText && searchText.length > 1) {
+        const foundData = data.filter((d: IUrl) => {
+          const containUrl = d.real_url.toLowerCase().includes(searchText.toLowerCase())
+          const containSlug = d.slug.toLowerCase().includes(searchText.toLowerCase())
+          return containSlug || containUrl
+        })
+        setFilteredData(foundData)
+      }
+    }
+  }, [isLoading, searchText, data])
 
   if (isLoading) {
     return <LoadingSkeleton />
+  }
+
+  const handleSearch = (e: ChangeEvent<HTMLInputElement>) => {
+    setSearchText(e.target.value)
   }
 
   return (
@@ -23,11 +46,19 @@ export function Items({ user, isFormVisible, onShowForm }: IUrlListProps) {
       {!isLoading && data && data.length > 0 ? (
         <>
           <TotalStats data={data} />
-          <List spacing={3}>
-            {data.map((urlItem: IUrl) => (
-              <Item data={urlItem} user={user} key={urlItem.id} />
-            ))}
-          </List>
+          <SearchInput onChangeSearch={handleSearch} searchText={searchText} />
+          {filteredData.length > 0 ? (
+            <List spacing={3}>
+              {filteredData.map((urlItem: IUrl) => (
+                <Item data={urlItem} user={user} key={urlItem.id} />
+              ))}
+            </List>
+          ) : (
+            <ErrorDataNotFound
+              useCta={false}
+              title="Tidak menemukan apapun nih, coba kata kunci lain!"
+            />
+          )}
         </>
       ) : (
         <>{!isFormVisible ? <ErrorDataNotFound useCta ctaAction={onShowForm} /> : null}</>
