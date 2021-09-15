b6f04530666efdad7092d8e4e4de1f6d632253e5
Brama Udi <bramaudi.al@gmail.com>
2021-09-16 03:11:56 +0700

    feat: add users count

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,7 @@
 import { Hero } from 'components/Hero'
 import { Features } from 'components/Features'
 import { CounterUrls } from 'components/CounterUrls'
+import { CounterUsers } from 'components/CounterUsers'
 import { MetaHead } from 'components/MetaHead/MetaHead'
 
 import { Layout } from 'components/Layout/Layout'
@@ -12,6 +13,7 @@ function Homepage() {
       <Hero />
       <Features />
       <CounterUrls />
+      <CounterUsers />
     </Layout>
   )
 }
