7bd6fd2c8b987679c31a63918edcc10f28a6b89c
Irfan Maulana (@mazipan) <maulana.irfan@tokopedia.com>
2022-04-02 16:41:21 +0700

    chore: use work-sans only

--- src/theme.ts
+++ src/theme.ts
@@ -1,30 +1,15 @@
-import { extendTheme, ChakraTheme } from '@chakra-ui/react'
-import { createBreakpoints, BaseBreakpointConfig } from '@chakra-ui/theme-tools'
+import { extendTheme } from '@chakra-ui/react'
 
-const breakpoints: BaseBreakpointConfig = createBreakpoints({
-  sm: '40em',
-  md: '52em',
-  lg: '64em',
-  xl: '80em'
-})
-
-// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-// @ts-ignore
-const theme: ChakraTheme = extendTheme({
+const theme = extendTheme({
   config: {
     initialColorMode: 'light',
     useSystemColorMode: false
   },
-  colors: {
-    black: '#16161D'
-  },
   fonts: {
     heading:
-      "'Playfair Display', ui-sans-serif, system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, 'Helvetica Neue', Arial, 'Noto Sans', sans-serif, 'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol', 'Noto Color Emoji'",
-    body: "'Work Sans', ui-sans-serif, system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, 'Helvetica Neue', Arial, 'Noto Sans', sans-serif, 'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol', 'Noto Color Emoji'",
-    mono: "ui-monospace, SFMono-Regular, Menlo, Monaco, Consolas, 'Liberation Mono', 'Courier New', monospace"
-  },
-  breakpoints
+      "'Work Sans', ui-sans-serif, system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, 'Helvetica Neue', Arial, 'Noto Sans', sans-serif, 'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol', 'Noto Color Emoji'",
+    body: "'Work Sans', ui-sans-serif, system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, 'Helvetica Neue', Arial, 'Noto Sans', sans-serif, 'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol', 'Noto Color Emoji'"
+  }
 })
 
 export default theme
